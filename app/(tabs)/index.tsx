a0c84a7dbac1392932ddb28de186398bd1444564
pratiksha-khairnar <pkhairnar175@gmail.com>
2026-04-26 11:47:26 +0530

    add initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-// Do baar peeche (../..), phir src, phir screens, phir home
 import HomeScreen from '../../src/screens/home';
 
 export default function Page() {
-  return (
-    <HomeScreen />
-  );
+  return <HomeScreen />;
 }
